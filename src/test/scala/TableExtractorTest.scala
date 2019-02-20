cdc9517489e33eb2b42e2e357a8fdff8043f75fb
Brandon Roberts <brandon@bxroberts.org>
2019-02-20 01:27:26 -0800

    Name the tests better

--- src/test/scala/TableExtractorTest.scala
+++ src/test/scala/TableExtractorTest.scala
@@ -10,13 +10,13 @@ class TableExtractorSuite extends FunSuite {
   var pdf: Document = PDF.open(testFile)
   var t = new TableExtractor(pdf)
 
-  test("Can identify the correct page") {
+  test("Can identify a simple page correctly") {
     var tableTitle = "Table 2.13 Sexual Assault Prevention Climate"
     var pageNumber = t.identifyPage(tableTitle)
     assert(pageNumber == 17)
   }
 
-  test("Can identify another correct page") {
+  test("Can identify a more difficult page correctly") {
     var tableTitle = "Figure 7. Respondents’ Knowledge of Military Attorney Eligibility."
     var pageNumber = t.identifyPage(tableTitle)
     assert(pageNumber == 21)
